708af7200b3182c5a30d68d1f9b2b0789d7a1241
Marcos Oliveira <41500739+terroo@users.noreply.github.com>
2023-05-10 19:18:17 -0300

    Add enemies and Bullter

--- core/src/com/terminalroot/game/SpaceShip.java
+++ core/src/com/terminalroot/game/SpaceShip.java
@@ -8,11 +8,21 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+import java.util.Iterator;
+
 public class SpaceShip extends ApplicationAdapter {
   SpriteBatch batch;
-  Texture img, tNave;
-  private Sprite nave;
-  private float posX, posY, velocity;
+  Texture img, tNave, tMissile, tEnemy;
+  private Sprite nave, missile;
+  private float posX, posY, velocity, xMissile, yMissile;
+  private boolean attack;
+  private Array<Rectangle> enemies;
+  private long lastEnemyTime;
 
   @Override
   public void create () {
@@ -23,17 +33,37 @@ public class SpaceShip extends ApplicationAdapter {
     posX = 0;
     posY = 0;
     velocity = 10;
+
+    tMissile = new Texture("missile.png");
+    missile  = new Sprite(tMissile);
+    xMissile = posX;
+    yMissile = posY;
+    attack = false;
+
+    tEnemy = new Texture("enemy.png");
+    enemies = new Array<Rectangle>();
+    lastEnemyTime = 0;
+    
   }
 
   @Override
   public void render () {
 
     this.moveNave();
+    this.moveMissile();
+    this.moveEnemies();
 
     ScreenUtils.clear(1, 0, 0, 1);
     batch.begin();
     batch.draw(img, 0, 0);
+    if(attack){
+      batch.draw(missile, xMissile + nave.getWidth() / 2, yMissile + nave.getHeight() / 2 - 12);
+    }
     batch.draw(nave, posX, posY);
+
+    for(Rectangle enemy : enemies ){
+      batch.draw(tEnemy, enemy.x, enemy.y);
+    }
     batch.end();
   }
 
@@ -63,6 +93,47 @@ public class SpaceShip extends ApplicationAdapter {
     if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
       if( posY > 0 ){
         posY -= velocity;
+      }
+    }
+  }
+
+  private void moveMissile(){
+    if( Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack ){
+      attack = true;
+      yMissile = posY;
+    }
+
+    if(attack){
+      if( xMissile < Gdx.graphics.getWidth() ){
+        xMissile += 40;
+      }else{
+        xMissile = posX;
+        attack = false;
+      }
+    }else{
+      xMissile = posX;
+      yMissile = posY;
+    }
+  }
+
+  private void spawnEnemies(){
+    Rectangle enemy = new Rectangle( Gdx.graphics.getWidth(), MathUtils.random(0, Gdx.graphics.getHeight() - tEnemy.getHeight()), tEnemy.getWidth(), tEnemy.getHeight());
+    enemies.add(enemy);
+    lastEnemyTime = TimeUtils.nanoTime();
+  }
+
+  private void moveEnemies(){
+
+    if( TimeUtils.nanoTime() - lastEnemyTime > 999999999 ){
+      this.spawnEnemies();
+    }
+
+
+    for( Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext(); ){
+      Rectangle enemy = iter.next();
+      enemy.x -= 400 * Gdx.graphics.getDeltaTime();
+      if(enemy.x + tEnemy.getWidth() < 0){
+        iter.remove();
       }
     }
   }
